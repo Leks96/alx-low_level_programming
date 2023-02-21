04d02df494578bcd00cfd0996694da0f9fda7ff2
Saidi Sodiq <saidisodiq96@gmail.com>
2023-02-21 21:35:08 +0100

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+* main - prints _putchar
+*
+*return: always - 0
+*/
+#include "main.h"
+int main(void)
+{
+	char *sh = "_putchar"
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar(\n);
+
+	return (0);
+}
+
