1f04bf5511bec6e96464469b36e2a01a046a680d
Saidi Sodiq <saidisodiq96@gmail.com>
2023-02-23 18:21:11 +0100

    debugging a C program

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,12 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;/* here the integer i is not needed for the required output */
+	/*i = 0;
 
-	while (i < 10)
+	while (i < 10) 
 	{
 		putchar(i);
-	}
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
