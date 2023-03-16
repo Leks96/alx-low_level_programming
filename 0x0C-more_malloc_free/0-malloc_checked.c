ae7a8217acec1dff64d2906df8b9e53d09488ce5
Saidi Sodiq <saidisodiq96@gmail.com>
2023-03-16 19:32:00 +0100

    concat 2 strings

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,11 @@
 void *malloc_checked(unsigned int b)
 {
 	void *s;
+
 	s = malloc(b);
 
-	if (s != NULL)
-		return (s);
-	else
+	if (s == NULL)
 		exit(98);
+
+	return (s);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* *string_nconcat - concatenates two strings.
+*
+* @s1: string 1
+* @s2: string 2
+* @n: integer input
+*
+* Return: concatenated strings
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *arr;
+	unsigned int i, j, count1, count2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
+	if (n < j)
+		j = n;
+
+	j += i;
+	arr = malloc(sizeof(*arr) * (j + 1));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (count1 = 0; count1 < i; count1++)
+		arr[count1] = s1[count1];
+	for (count2 = 0; count2 < j; count2++)
+	{
+		arr[count1] = s2[count2];
+		count1++;
+	}
+	count1++;
+	arr[count1] = '\0';
+	return (arr);
+}
