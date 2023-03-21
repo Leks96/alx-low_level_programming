8c83b8c337c7f33dbb33f983051239545d899a38
Saidi Sodiq <saidisodiq96@gmail.com>
2023-03-21 01:13:03 +0100

    fixed some errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->age <= 0)
-		d->age = "(nil)";
+		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
