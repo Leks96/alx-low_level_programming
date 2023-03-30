25e1c12723857432e366b1f4c819c8454f50b7da
Saidi Sodiq <saidisodiq96@gmail.com>
2023-03-30 23:20:51 +0100

    fixed error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,15 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
+	if (!h)
+		return (0);
+
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str)
+			printf("[%d] %s\n", h->len, h->str)
 
 		nodes++;
 		h = h->next;
