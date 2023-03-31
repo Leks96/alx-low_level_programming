adbbae56262a04d7e8623031f99ab4ca32c258f0
Saidi Sodiq <saidisodiq96@gmail.com>
2023-03-31 07:42:49 +0100

    fixed compilation error

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,21 +10,11 @@
 */
 void free_list(list_t *head)
 {
-	list_t actual_node;
-	list_t next_node;
+	if (!head)
+		return;
 
-	if (head)
-	{
-		actual_node = head;
-		next_node = head->next;
-		while (next_node)
-		{
-			free(actual_node->str);
-			free(actual_node);
-			actual_node = next_node;
-			next_node = next_node->next;
-		}
-		free(actual_node->str);
-		free(actual_node);
-	}
+	free_list(head->next);
+	if (head->str)
+		free(head->str);
+	free(head);
 }
