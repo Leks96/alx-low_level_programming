5a6c32158963b938a2bc33715fe9cef5cae06d0b
Saidi Sodiq <saidisodiq96@gmail.com>
2023-03-31 07:15:05 +0100

    fixed library issues

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
 /**
 * print_list - a function that prints
 * all the elements of a list_t list.
