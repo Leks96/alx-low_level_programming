46de7452d4e053c76e1caaa455f8b69e0c6f5e48
Saidi Sodiq <saidisodiq96@gmail.com>
2023-10-26 00:56:34 +0100

    initial commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
-* _sqrt_recursion - returns the natural square root of a number.
-* @n: the number to which the natural sqr root is to be returned
-* Return: the square root of the number
-*/
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	return (find_sqrt(n, 1));
-}
-
-/**
 * find_sqrt - helper function to find the square root recursively
 * @n: the number for which to find the natural square root
 * @x: the guess for the square root
@@ -25,4 +13,16 @@ int find_sqrt(int x, int n)
 		return (-1);
 	else
 		return (find_sqrt(n, x + 1));
+}
+
+/**
+* _sqrt_recursion - returns the natural square root of a number.
+* @n: the number to which the natural sqr root is to be returned
+* Return: the square root of the number
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (find_sqrt(n, 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
-* is_prime_number - check if an integer is prime
-* @n: the integer to be checked
-* Return: 1 if n is prime, or 0 if not
-*/
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (is_prime_recursive(n, 2));
-}
-
-/**
 * check_prime - helper functio to help find the square root
 * @n: the number to be checked for primality
 * @x: the divisor
@@ -24,4 +12,16 @@ int is_prime_recursive(int n, int x)
 	if (n % x == 0)
 		return (0);
 	return (is_prime_recursive(n, x + 1));
+}
+
+/**
+* is_prime_number - check if an integer is prime
+* @n: the integer to be checked
+* Return: 1 if n is prime, or 0 if not
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_recursive(n, 2));
 }
