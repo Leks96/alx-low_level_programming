0251084223d121aa439d0e4398c7595cd7f60945
Saidi Sodiq <saidisodiq96@gmail.com>
2023-11-08 05:41:57 +0100

    initial commit

---
+++ 0x0E-structures_typedef/0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints the name of the source file, followed by a new line.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	    printf("%s\n", __FILE__);
+	        return (0);
+}
